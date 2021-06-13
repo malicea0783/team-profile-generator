6556159ed6a1c9a15c98828018196ce1ecc642b7
Miguel Alicea <malicea0783@gmail.com>
2021-06-12 19:09:59 -0500

    finished testing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require('./../lib/Employee');
+describe('Employee class', () => {
+    const e = new Employee(1, 'Valerie Michalecki', 'valerie227@gmail.com');
+    it('Creates an employee object', () => {
+        expect(typeof (e)).toBe('object');
+    })
+    it('Has an id attribute', () => {
+        expect(e.id).toBe(1);
+    });
+    it('Has a name attribute', () => {
+        expect(e.name).toBe('Valerie Michalecki');
+    });
+    it('Has an email attribute', () => {
+        expect(e.email).toBe('valerie227@gmail.com');
+    });
+    it('Can get ID via getId()', () => {
+        expect(e.getId()).toBe(1);``
+    });
+    it('Can get name via getName()', () => {
+        expect(e.getName()).toBe('Valerie Michalecki');
+    });
+    it('Can get email via getEmail()', () => {
+        expect(e.getEmail()).toBe('valerie227@gmail.com');
+    });
+    it('Can get role via getRole()', () => {
+        expect(e.getRole()).toBe('Employee');
+    });
+});
---
+++ index.js
@@ -0,0 +1,149 @@
+// emplouyee classes
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+// prompt for information
+const inquirer = require('inquirer')
+// file paths
+const path = require('path');
+const fs = require('fs');
+// creates folder for generated html
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+// html template
+const generateHTML = require('./src/html-template')
+// each employee object will push into this array
+const teamMembers = [];
+// create manager with prompts
+function createManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is the team manager\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'What is the team manager\'s ID?'
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is the team manager\'s email address?'
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: 'What is the team manager\'s office number?'
+        },
+    ]).then(answers => {
+        // create manager object
+        const manager = new Manager(answers.managerId, answers.managerName, answers.managerEmail, answers.managerOfficeNumber);
+        // push manager to team
+        teamMembers.push(manager);
+        // ask user to create another employee
+        createTeam();
+    });
+}
+// create engineer with prompts
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the engineer\'s name?'
+            // add validator ('You must enter xyz')
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: 'What is the engineer\'s ID?'
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the engineer\'s email address?'
+        },
+        {
+            type: 'input',
+            name: 'engineerGitHub',
+            message: 'What is the engineer\'s GitHub?'
+        },
+    ]).then(answers => {
+        // create engineer object
+        const engineer = new Engineer(answers.engineerId, answers.engineerName, answers.engineerEmail, answers.engineerGitHub);
+        // push engineer to team
+        teamMembers.push(engineer);
+        createTeam();
+    });
+}
+// create intern with prompts
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the intern\'s name?'
+            // add validator ('You must enter xyz')
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'What is the intern\'s id?'
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the intern\'s email?'
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What is the intern\'s school?'
+        },
+    ]).then(answers => {
+        // create intern object
+        const intern = new Intern(answers.internId, answers.internName, answers.internEmail, answers.internSchool);
+        // push intern to team
+        teamMembers.push(intern);
+        createTeam();
+    });
+}
+// create team with prompts
+function createTeam() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberChoice',
+            message: 'What team member would you like to create?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'I\'m done creating my team'
+            ]
+        },
+    ]).then(userChoice => {
+        if (userChoice.memberChoice === 'Engineer') {
+            createEngineer();
+        } else if (userChoice.memberChoice === 'Intern') {
+            createIntern();
+        } else {
+            buildTeam();
+        }
+    }
+    )
+}
+function buildTeam() {
+    // create html file
+    console.log(teamMembers);
+    const htmlPageContent = generateHTML(teamMembers);
+    createFile(htmlPageContent);
+}
+function createFile(htmlCode) {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, htmlCode, 'utf-8');
+}
+createManager();
